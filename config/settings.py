cc6619299f078b8ea9164470b9de2222c809d262
Shivathamme <147991564+Shivathamme@users.noreply.github.com>
2026-06-21 19:11:53 +0530

    Delete config directory

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv('gemini_class')
-
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY ot found in .env file")
